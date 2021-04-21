55ecaebe4b0d152017dc4b8ac74f8dd68f1c2fc0
koverchik <koverchik-work@yandex.ru>
2021-04-21 23:52:33 +0300

    add: button for change catchegory

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
 const topic = ["Как дела?", "Как настроение?", "Расскажи о себе", "Чем увлекаешься?", "Как твой день проходит?", "Чем занимаешься", "Что тут ищешь?", "Где вы живете?", "Как давно тут?", "Как настроение"];
-const imgEmogi = ["fanny.svg", "sarcasm.svg", "serios.svg", "sweet.svg"];
+const emotion = ["Смешной", "Сарказм", "Серьезный", "Ровно"];
 
 function cangeDom(){
     const idElem = document.getElementsByTagName('body');
@@ -20,7 +20,30 @@ function cangeDom(){
      };
 
     idElem[0].append(blockExtension);
-    
+
+    function clickOnElement(e){
+            
+        const blockWithName = e.target.parentNode;
+        let listAll = blockWithName.parentNode.getElementsByTagName('ol');
+   
+        for (i = 0; i < listAll.length; i++) {
+            listAll[i].style.display = 'none';
+           
+         }
+        const elemClick = document.getElementById(e.target.getAttribute("name-data"));
+        console.log( elemClick.style.display);
+        console.log( elemClick.getAttribute('display'));
+        
+        // console.log(window.getComputedStyle(elemClick).getPropertyValue('display'));
+        elemClick.style.display = 'flex';
+        elemClick.style.flexDirection = 'column';
+        elemClick.style.overflow = 'scroll';
+        elemClick.style.height = document.documentElement.clientHeight + "px";
+       
+
+         
+
+    }
    function createOneTheme (arrayItem){
     // Создание категории (вопросов)
     let helerElem = document.createElement('div');
@@ -29,11 +52,23 @@ function cangeDom(){
     helerElem.setAttribute('topic-data', 1);
     blockExtension.append(helerElem);
 
+    let wrapperEmotions = document.createElement('div');
+    blockExtension.append(wrapperEmotions);
+    wrapperEmotions.setAttribute('class', 'wrapperButtonEmotion');
+    
     arrayItem.forEach((item, i)=>{
      
         // Создание списка
+        let buttonElem = document.createElement('div');
+        buttonElem.innerHTML = emotion[i];
+        buttonElem.setAttribute('class', 'buttonEmotion');
+        buttonElem.setAttribute('name-data', emotion[i]);
+        wrapperEmotions.append(buttonElem);
+
+        buttonElem.addEventListener( "click" , clickOnElement);
 
         let wtapperElem = document.createElement('ol');
+        wtapperElem.setAttribute('id', emotion[i]);
         wtapperElem.append(createElement(item));
         wtapperElem.setAttribute('style', 'display: none');
       
