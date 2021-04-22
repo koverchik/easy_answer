f591ec721dc9b0d6b8ed6b8e1a229e2a7cea00ee
koverchik <koverchik-work@yandex.ru>
2021-04-22 17:16:02 +0300

    add: multiple sections

--- extension.js
+++ extension.js
@@ -1,5 +1,9 @@
 const topic = ["Как дела?", "Как настроение?", "Расскажи о себе", "Чем увлекаешься?", "Как твой день проходит?", "Чем занимаешься", "Что тут ищешь?", "Где вы живете?", "Как давно тут?", "Как настроение"];
-const emotion = ["Смешной", "Сарказм", "Серьезный", "Ровно"];
+const howAreYouTopic = ["#Смешной", "#Сарказм", "#Серьезный", "#Ровно"];
+const howAreYourMoodTopic = ["#Ну_такое", "#Всё_хорошо", "#Веселый", "#Лучше"]
+const aboutYourselfTopic = ["#Милая", "#С_характером", "#Самая_самая"];
+const areYouHobbiesTopic = ["#Несерьезно", "#Приличная", "#Короткий"];
+const howIsYourDayTopic = ["#Все_хорошо", "#Не_очень", "#C_шуткой", "#Пичаль"];
 
 function cangeDom(){
     const idElem = document.getElementsByTagName('body');
@@ -25,60 +29,69 @@ function cangeDom(){
             
         const blockWithName = e.target.parentNode;
         let listAll = blockWithName.parentNode.getElementsByTagName('ol');
-   
-        for (i = 0; i < listAll.length; i++) {
-            listAll[i].style.display = 'none';
-           
-         }
         const elemClick = document.getElementById(e.target.getAttribute("name-data"));
-        console.log( elemClick.style.display);
-        console.log( elemClick.getAttribute('display'));
-        
-        // console.log(window.getComputedStyle(elemClick).getPropertyValue('display'));
-        elemClick.style.display = 'flex';
-        elemClick.style.flexDirection = 'column';
-        elemClick.style.overflow = 'scroll';
-        elemClick.style.height = document.documentElement.clientHeight + "px";
+   
+
+
+         if(elemClick.classList.contains('active')){
+            elemClick.classList.remove('active');
+            elemClick.style.display = 'none';
+            
+         }else{
+            for (i = 0; i < listAll.length; i++) {
+                listAll[i].style.display = 'none';
+                listAll[i].classList.contains('active') ? listAll[i].classList.remove('active'): "";
+               
+              }
+             elemClick.style.display = 'flex';
+             elemClick.classList.add('active');
+             elemClick.style.flexDirection = 'column';
+             elemClick.style.overflow = 'scroll';
+             elemClick.style.height = document.documentElement.clientHeight + "px";
+         }
        
-
-         
-
+      
     }
-   function createOneTheme (arrayItem){
+
+   function createOneTheme (arrayItem, categoryName, classAnswer){
     // Создание категории (вопросов)
+    let wrapperAllList = document.createElement('div');
     let helerElem = document.createElement('div');
-    helerElem.innerHTML = "Как дела?";
+    helerElem.innerHTML = classAnswer;
     helerElem.setAttribute('class', 'header-item');
-    helerElem.setAttribute('topic-data', 1);
-    blockExtension.append(helerElem);
+    helerElem.setAttribute('topic-data', classAnswer);
+    wrapperAllList.append(helerElem);
 
     let wrapperEmotions = document.createElement('div');
-    blockExtension.append(wrapperEmotions);
+    wrapperAllList.append(wrapperEmotions);
+
     wrapperEmotions.setAttribute('class', 'wrapperButtonEmotion');
-    
+    blockExtension.append(wrapperAllList);
     arrayItem.forEach((item, i)=>{
      
         // Создание списка
         let buttonElem = document.createElement('div');
-        buttonElem.innerHTML = emotion[i];
+        buttonElem.innerHTML = categoryName[i];
         buttonElem.setAttribute('class', 'buttonEmotion');
-        buttonElem.setAttribute('name-data', emotion[i]);
+        buttonElem.setAttribute('name-data', categoryName[i]);
         wrapperEmotions.append(buttonElem);
 
         buttonElem.addEventListener( "click" , clickOnElement);
 
         let wtapperElem = document.createElement('ol');
-        wtapperElem.setAttribute('id', emotion[i]);
+        wtapperElem.setAttribute('id', categoryName[i]);
         wtapperElem.append(createElement(item));
         wtapperElem.setAttribute('style', 'display: none');
       
-        blockExtension.append(wtapperElem);
+        wrapperAllList.append(wtapperElem);
       })
    }
-   createOneTheme(howAreYou);
-  
 
-
+   createOneTheme(howAreYou, howAreYouTopic, "Как дела?");
+   createOneTheme(howAreYourMood, howAreYourMoodTopic, "Как настроение?");
+   createOneTheme(aboutYourSelf, aboutYourselfTopic, "Расскажи о себе");
+   createOneTheme(areYouHobbies, areYouHobbiesTopic, "Чем увлекаешься?");
+   createOneTheme(howIsYourDay , howIsYourDayTopic, "Как твой день проходит?");
 }
 
 document.addEventListener('readystatechange', cangeDom);
